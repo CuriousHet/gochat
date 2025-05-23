50a4dbd3e52fd48004e4b1f0af825978f90667d0
CuriousHet <het18211@gmail.com>
2025-05-23 07:24:58 +0530

    websocket + chatroom

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"server/db"
 	"server/internal/user"
+	"server/internal/ws"
 	"server/router"
 )
 
@@ -18,6 +19,10 @@ func main() {
 	userSvc := user.NewService(userRep)
 	userHandler := user.NewHandler(userSvc)
 
-	router.InitRouter(userHandler)
+	hub := ws.NewHub()
+	wsHandler := ws.NewHandler(hub)
+	go hub.Run()
+
+	router.InitRouter(userHandler, wsHandler)
 	router.Start("0.0.0.0:8080")
 }
